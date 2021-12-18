20e710e95447e21bc2d7471fd5de2fbf0423a5d4
Rushiahire <rushiahire9567@gmail.com>
2021-12-18 01:10:57 -0600

    updated

--- src/products/DetailView.jsx
+++ src/products/DetailView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box , Typography,Rating, Button} from '@mui/material';
+import { Box, Typography, Rating, Button, Grid } from '@mui/material';
 import mob from '../images/mob.webp';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
@@ -8,43 +8,73 @@ import RedeemOutlinedIcon from '@mui/icons-material/RedeemOutlined';
 const DetailView = () => {
 
     const detailImageStyle = {
-        width :{
-            md : '50%',
-            sm : '60%',
-            xs : '90%'
+        width: {
+            md: '50%',
+            sm: '60%',
+            xs: '90%'
         },
-        display : 'block',
-        mx : {
-            md : '10px',
-            sm : 'auto'
+        display: 'block',
+        mx: {
+            md: '10px',
+            sm: 'auto'
         }
     }
 
-    return(
-        <>
-            <Box>
+    // return(
+    //     <>
+    //         <Box>
+    //             <Box component='img' src={mob} sx={detailImageStyle} />
+    //             <Box component="div" >
+    //                 <Typography variant='h4'>Iphone 13 mini (128gb) Blue</Typography>
+    //                 <Rating name="read-only" value={4} readOnly />
+    //                 <Typography>Price : &#8377;69,900.00</Typography>
+    //                 <Typography>Model Name : Iphone</Typography>
+    //                 <Typography>Brand : Apple</Typography>
+    //                 <Typography>Storage : 128gb</Typography>
+    //                 <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestiae in libero accusamus quisquam nobis, obcaecati, animi magni asperiores, architecto cum fugiat voluptate. Repudiandae, molestiae.</Typography>
+    //             </Box>
+
+    //             <Box>
+    //                 <Box>
+    //                     <Button variant='contained'> <ShoppingBagOutlinedIcon/> Buy now</Button>
+    //                     <Button variant='contained'> <AddShoppingCartOutlinedIcon/> Add to Cart  </Button>
+    //                     <Button variant='contained' >  <RedeemOutlinedIcon/> Add to Wish List </Button>
+    //                 </Box>
+    //                 <Box></Box>
+    //                 <Box></Box>
+    //             </Box>
+    //         </Box>
+    //     </>
+    // )
+
+    return (
+        <Grid container >
+            <Grid item md={6} >
                 <Box component='img' src={mob} sx={detailImageStyle} />
                 <Box component="div" >
                     <Typography variant='h4'>Iphone 13 mini (128gb) Blue</Typography>
                     <Rating name="read-only" value={4} readOnly />
-                    <Typography>Price : &#8377;69,900.00</Typography>
+                    <Typography>Price : &#8377;69,900.00 </Typography>
                     <Typography>Model Name : Iphone</Typography>
                     <Typography>Brand : Apple</Typography>
                     <Typography>Storage : 128gb</Typography>
                     <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestiae in libero accusamus quisquam nobis, obcaecati, animi magni asperiores, architecto cum fugiat voluptate. Repudiandae, molestiae.</Typography>
                 </Box>
-                
+
+            </Grid>
+            <Grid item md={6}>
                 <Box>
-                    <Box>
-                        <Button variant='contained'> <ShoppingBagOutlinedIcon/> Buy now</Button>
-                        <Button variant='contained'> <AddShoppingCartOutlinedIcon/> Add to Cart  </Button>
-                        <Button variant='contained' >  <RedeemOutlinedIcon/> Add to Wish List </Button>
-                    </Box>
-                    <Box></Box>
-                    <Box></Box>
+                    <Button variant='contained'> <ShoppingBagOutlinedIcon /> Buy now </Button>
+                    <Button variant='contained'> <AddShoppingCartOutlinedIcon /> Add to Cart  </Button>
+                    <Button variant='contained' >  <RedeemOutlinedIcon /> Add to Wish List </Button>
                 </Box>
-            </Box>
-        </>
+                <Box>
+
+                </Box>
+                <Box></Box>
+
+            </Grid>
+        </Grid>
     )
 }
 
