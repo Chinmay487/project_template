b9d76d0e9e1b84fa30dcfcc41aa5012028ef723e
Rushiahire <rushiahire9567@gmail.com>
2021-12-19 04:34:15 -0600

    detail donee

---
+++ src/products/Review.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box,Typography, Rating } from '@mui/material';
+
+
+const Review = () => {
+    return (
+        <Box component='div' sx={{ padding: '2%' }}>
+            <Typography variant='subtitle2'>
+                Username
+            </Typography >
+            <Rating name="read-only" value={4} />
+            <Typography variant='h6'>
+                Title
+            </Typography>
+            <Typography Variant='p'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio iure dolorum, necessitatibus asperiores magnam praesentium debitis ea qui. Similique, eveniet?
+            </Typography>
+        </Box>
+    )
+
+
+}
+
+
+export default Review;
