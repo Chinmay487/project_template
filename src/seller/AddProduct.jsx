70cb413e69a3122924b6d35146a0389d709ae291
Rushiahire <rushiahire9567@gmail.com>
2021-12-19 07:45:00 -0600

    add product form started

--- src/seller/AddProduct.jsx
+++ src/seller/AddProduct.jsx
@@ -5,7 +5,15 @@ import { Box ,Typography ,Button ,TextField  } from '@mui/material';
 const AddProduct = () => {
     return (
         <Box component='form'>
-            
+                <TextField id='standard-basic' variant='standard' label='Enter the Product Name' />
+                <TextField  id='outlined-basic' variant='outlined' label='Description' />
+                <Typography>
+                < TextField id='filled-basic' variant='filled' label='Price'/>
+                <TextField id='filled-basic' variant='filled' label='Discount Price'/>
+                </Typography>
+                <Typography variant='h6'>
+                    How much quantity Available 
+                </Typography>
         </Box>
     )
 }
