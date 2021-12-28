cc09e773bb6a2d6f84472675a3316975d0682fe1
Rushiahire <rushiahire9567@gmail.com>
2021-12-28 22:02:05 +0530

    delete done

--- src/seller/Panel.jsx
+++ src/seller/Panel.jsx
@@ -1,16 +1,79 @@
-import React from 'react';
-import { Grid, Typography } from '@mui/material'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Grid, Typography, CircularProgress } from '@mui/material'
 import History from '../user/History';
+import axios from 'axios';
+import { Box } from '@mui/system';
+
+
 
 const Panel = (props) => {
+
+    const [dataList, setDataList] = useState([]);
+
+    const [len, setLen] = useState(false)
+
+    const fetchData = useCallback(() => {
+        axios.get('http://192.168.43.65:8000/seller/panel')
+            .then((response) => response.data)
+            .then((data) => {
+                if (data.length > 0){
+                    setDataList([...data])
+                    
+                }
+                else{
+                    setLen(data.length > 0);
+                    
+                }
+                
+            })
+            .catch((error) => {
+                alert('something went wrong')
+            })
+    }, [])
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
+
+
     return (
         <>
             <Typography variant="h4" align="center" sx={{ my: "2%" }}>
                 Your Products
             </Typography>
-            <Grid container rowGap={2} sx={{width:{lg:"80%",md:"80%",sm:"90%",xs:"90%"},mx:"auto"}} >
-                <History isCart={false} isSeller={props.isSeller}   />
-            </Grid>
+
+            {
+                (!dataList.length > 0 ) ? <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Typography variant='h5'>
+                        Fetching your products
+                    </Typography>
+                    <CircularProgress />
+                </Box> :
+
+                    <>
+                        {
+
+                            !len ?
+                                <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <Typography variant='h5'>
+                                        No products added
+                                    </Typography>
+
+                                </Box>
+                                :
+                                <Grid container rowGap={3} sx={{ width: { lg: "70%", md: "70%", sm: "90%", xs: "90%" }, mx: "auto" }} >
+                                    {
+                                        dataList.map((item) => {
+                                            return (
+                                                <History isCart={false} isSeller={props.isSeller} item={item} key={item.key} />
+                                            )
+                                        })
+                                    }
+                                </Grid>
+                        }
+                    </>
+            }
         </>
     )
 }
--- src/user/History.jsx
+++ src/user/History.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Grid, Typography, Box, useTheme, Button } from '@mui/material';
-import jcb from '../images/jcb.jpg';
+import React, {useState} from 'react';
+import { Grid, Typography, Box, useTheme, Button , CircularProgress} from '@mui/material';
 import { Link } from 'react-router-dom';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 
 const History = (props) => {
 
     const theme = useTheme()
+
+    const [deleteButtonState , setDeleteButtonState] = useState(false)
 
     const profileGridItem = {
         display: "flex",
@@ -67,7 +69,23 @@ const History = (props) => {
     const navigate = useNavigate()
 
     const gotoUpdate = () => {
-        navigate('/update/3')
+        navigate(`/update/${props.item.key}`)
+    }
+
+
+    const deleteProduct =()=>{
+        setDeleteButtonState(true)
+        const data = new FormData()
+        data.append('id',props.item.key)
+        axios.post('http://192.168.43.65:8000/seller/deleteproduct',data)
+        .then((response)=>{
+            setDeleteButtonState(false)
+            alert(response.data)
+            window.location.reload();
+        })
+        .catch((error)=>{
+            alert('Something went wrong')
+        })
     }
 
     return (
@@ -75,15 +93,15 @@ const History = (props) => {
             <Grid container sx={{ boxShadow: theme.shadows[5], backgroundColor: '#F5F5F5' }} >
                 <Grid item md={3} sm={12} xs={12} >
                     <Box component="center">
-                        <Box component="img" src={jcb} sx={{ maxWidth: { lg: "80%", md: "80%", sm: "80%", xs: "80%" } }} />
+                        <Box component="img" src={props.item.thumbnail} sx={{ maxWidth: { lg: "80%", md: "80%", sm: "80%", xs: "80%" } , height:'10rem'}} />
                     </Box>
                 </Grid>
                 <Grid item md={7} sm={12} xs={12} sx={profileGridItem} >
                     <Link to='/detail/3' className='link'>
                         <Typography sx={profileGridItemText} variant="h6" >
-                            JCB 13 mini (128gb) yellow </Typography>
+                            {props.item.title}</Typography>
                     </Link>
-                    <Typography sx={profileGridItemText} variant="h6"> Price : 69,999 </Typography>
+                    <Typography sx={profileGridItemText} variant="h6"> Price : {props.item.price} </Typography>
                     {
                         props.is_cart ? <Box sx={quantity}>
                             <Typography variant='h6'> Qty : &nbsp;</Typography>
@@ -93,22 +111,28 @@ const History = (props) => {
                                 <Box component="option" value={3} selectteed>3</Box>
                                 <Box component="option" value={4} selectteed>4</Box>
                                 <Box component="option" value={5} selectteed>5</Box>
-                                <Box component="option" value={6} selectteed>6</Box>
                                 <Box component="option" value={7} selectteed>7</Box>
+                                <Box component="option" value={6} selectteed>6</Box>
                                 <Box component="option" value={8} selectteed>8</Box>
                                 <Box component="option" value={9} selectteed>9</Box>
                                 <Box component="option" value={10} selectteed>10</Box>
                             </Box>
-                        </Box> : <Typography sx={profileGridItemText} variant="h6"> Qty : 12 </Typography>
+                        </Box> : <Typography sx={profileGridItemText} variant="h6"> Qty : {props.item.quantity} </Typography>
                     }
                 </Grid>
                 <Grid item md={2} sm={12} xs={12} sx={profileGridItem}>
                     {
                         (props.is_cart || props.isSeller) ?
                             <Box sx={cartButtons}>
-                                <Button variant='text' color='error' sx={{ width:`${props.isSeller ? '100%' : '30%'}`,height : `${props.isSeller ? "auto":"2rem"}` }}>
+                                {
+                                    deleteButtonState ?
+                                    <Box  sx={{ width:'100%' , display:'flex' , justifyContent:'center',alignItems:'center' , my:'1rem'}}> 
+                                    <CircularProgress/>
+                                    </Box> : 
+                                <Button variant='text' onClick={deleteProduct} color='error' sx={{ width:`${props.isSeller ? '100%' : '30%'}`,height : `${props.isSeller ? "auto":"2rem"}` }}>
                                     <DeleteForeverIcon sx={{ color: "red" }} />
                                 </Button>
+                                }
                                 {
                                     props.isSeller ? <Button variant="outlined" onClick={gotoUpdate} sx={{width:{lg:"50%",md:"50%",sm:"60%",xs:"60%"},mx:"auto"}}  >Update</Button> : null
                                 }
