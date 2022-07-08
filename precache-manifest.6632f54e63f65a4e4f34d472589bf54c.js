d4d9192c26e6c012c1a34ca0d790bb50cea0f467
Chinmay487 <pchinmay04@gmail.com>
2022-07-08 21:11:44 +0530

    Updates

---
+++ precache-manifest.6632f54e63f65a4e4f34d472589bf54c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "daf15cc35fdfbbbb5692",
+    "url": "/project_template/static/css/main.58418a56.chunk.css"
+  },
+  {
+    "revision": "daf15cc35fdfbbbb5692",
+    "url": "/project_template/static/js/main.daf15cc3.chunk.js"
+  },
+  {
+    "revision": "65172f2710da5fdaf55e",
+    "url": "/project_template/static/js/1.65172f27.chunk.js"
+  },
+  {
+    "revision": "cfd520ab68c073e4ec68",
+    "url": "/project_template/static/js/runtime~main.cfd520ab.js"
+  },
+  {
+    "revision": "792e21275338686c5c72ff4ba27243db",
+    "url": "/project_template/index.html"
+  }
+];
